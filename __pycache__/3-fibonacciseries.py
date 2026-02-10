70e7019a3f264aeadcf22638598564e261fabdf5
Sai Amulya <yellapragadasaiamulya@gmail.com>
2026-02-10 12:48:57 -0500

    3-fibonacci brute force

---
+++ __pycache__/3-fibonacciseries.py
@@ -0,0 +1,21 @@
+def main():
+    n = int(input())
+    fib = [0] * (n+1)
+    if n == 0:
+        print("Fibonacci series till 0th term is:")
+        print(0)
+    elif n == 1:
+        print("Fibonacci seried till 1st term is:")
+        print("0 1")
+    else:
+        fib[0] = 0
+        fib[1] = 1
+        for i in range(2,n+1):
+            fib[i] = fib[i-1] + fib[i-2]
+        print(f"Fibonacci series till {n}th term is:")
+        print(" ".join(str(num) for num in fib))
+
+if __name__ == "__main__":
+    main()
+
+
